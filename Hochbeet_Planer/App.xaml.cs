9a28d83f0ee5e90dd6c0a50e5ed395f3f1b79214
Chrissi <Chrissi@LAPTOP-V3PLIGU4>
2026-05-24 13:10:03 +0200

    Projektdateien hinzufügen.

---
+++ Hochbeet_Planer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Hochbeet_Planer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
