da53194ea5251794b494b66f43ad89fe006a2560
Christina Gass <20232693@htl-md.schule>
2026-05-25 20:14:52 +0200

    feat(data): fix SQLite DLL dependency

--- Hochbeet_Planer/MainWindow.xaml.cs
+++ Hochbeet_Planer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Hochbeet_Planer.Data;
 using Hochbeet_Planer.Models;
 using System.Data.SQLite;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,13 +24,15 @@ namespace Hochbeet_Planer
         private Border[,] zellenGrid;
         private Pflanze ausgewaehltePflanze;
         private List<Pflanze> pflanzenListe;
+        private Dictionary<string, string> beetBelegung = new Dictionary<string, string>();
 
 
-        public MainWindow()
+        public MainWindow() 
         {
             InitializeComponent();
             PflanzenLaden(); //PflanzenObjekte laden (später vl Datenbank dazu?)
             DatabaseHelper.InitializeDatabase(); //Datenbank laden für Beet speichern und laden
+            BeeteAnzeigen();
         }
 
         private void BeetGenerieren(int breite, int laenge)
@@ -119,13 +122,13 @@ namespace Hochbeet_Planer
             if (zeile + ausgewaehltePflanze.LaengeInZellen > zellenGrid.GetLength(0))
             {
                 MessageBox.Show(ausgewaehltePflanze.Name + " passt hier nicht rein!\n" +
-                    "Größe: " + ausgewaehltePflanze.BreiteInZellen +
+                    "Größe(bxl): " + ausgewaehltePflanze.BreiteInZellen +
                     " x " + ausgewaehltePflanze.LaengeInZellen + " Zellen"); return;//return damit Programm nicht mehr abstürzt
             }
             if (spalte + ausgewaehltePflanze.BreiteInZellen > zellenGrid.GetLength(1))
             {
                 MessageBox.Show(ausgewaehltePflanze.Name + " passt hier nicht rein!\n" +
-                    "Größe: " + ausgewaehltePflanze.BreiteInZellen +
+                    "Größe(bxl): " + ausgewaehltePflanze.BreiteInZellen +
                     " x " + ausgewaehltePflanze.LaengeInZellen + " Zellen"); return;
             }
 
@@ -133,10 +136,12 @@ namespace Hochbeet_Planer
             {
                 for (int i = spalte; i < spalte + ausgewaehltePflanze.BreiteInZellen; i++)
                 { 
-                zellenGrid[j, i].Background = new SolidColorBrush(Color.FromRgb
+                    zellenGrid[j, i].Background = new SolidColorBrush(Color.FromRgb
                                                             (ausgewaehltePflanze.FarbeR,
                                                             ausgewaehltePflanze.FarbeG,
                                                             ausgewaehltePflanze.FarbeB));
+                    
+                    beetBelegung[j + "_" + i] = ausgewaehltePflanze.Name;
                 }
             }
         }
@@ -170,7 +175,7 @@ namespace Hochbeet_Planer
                 //Beet speichern
                 string sql = @"
                     INSERT INTO Beete (Name, Breite, Laenge)
-                    VALUES (@Name, @Breite, @Laenge)";
+                    VALUES (@Name, @Breite, @Laenge)"; //@zum dynamischen befüllen später
 
                 using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
                 {
@@ -179,8 +184,123 @@ namespace Hochbeet_Planer
                     cmd.Parameters.AddWithValue("@Laenge", grdHochbeet.RowDefinitions.Count);
                     cmd.ExecuteNonQuery();
                 }
+
+                //id vom gespeicherten Beet holen long oder int
+                int beetId = (int) conn.LastInsertRowId; 
+
+                foreach(KeyValuePair<string, string> eintrag in beetBelegung)
+                {
+                    string[] teile = eintrag.Key.Split('_');
+                    int zeile = int.Parse(teile[0]);
+                    int spalte = int.Parse(teile[1]);
+
+                    string sqlBelegung = @"INSERT INTO BeetBelegung (BeetId, Zeile, Spalte, PflanzenName)
+                                          VALUES (@BeetId, @Zeile, @Spalte, @PflanzenName)
+                                         "; //@zum dynamischen befüllen später
+                    using (SQLiteCommand cmdBelegung = new SQLiteCommand(sqlBelegung, conn) )
+                    {
+                        cmdBelegung.Parameters.AddWithValue("@BeetId", beetId);
+                        cmdBelegung.Parameters.AddWithValue("@Zeile", zeile);
+                        cmdBelegung.Parameters.AddWithValue("@Spalte", spalte);
+                        cmdBelegung.Parameters.AddWithValue("@PflanzenName", eintrag.Value);
+                        cmdBelegung.ExecuteNonQuery();
+
+                    }
+
+                }
             }
             MessageBox.Show("Beet gespeichert :)");
+            BeeteAnzeigen();
         }
+        private void BeeteAnzeigen()
+        {
+            lstBeete.Items.Clear();
+
+            using (SQLiteConnection conn = new SQLiteConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+                string sql = "SELECT Name FROM Beete";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                {
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lstBeete.Items.Add(reader["Name"].ToString());
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void btnLaden_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstBeete.SelectedItem == null)
+            {
+                MessageBox.Show("Bitte ein Beet aus der Liste auswählen!");
+                return;
+            }
+
+            string beetName = lstBeete.SelectedItem.ToString();
+
+            using (SQLiteConnection conn = new SQLiteConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+
+                // Beet laden
+                string sql = "SELECT * FROM Beete WHERE Name = @Name";
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Name", beetName);
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            int breite = (int)(long)reader["Breite"];
+                            int laenge = (int)(long)reader["Laenge"];
+                            BeetGenerieren(breite * zellenGroesse, laenge * zellenGroesse);
+                        }
+                    }
+                }
+                //Belegung laden
+                int beetId2 = 0;
+                string sqlId = "SELECT Id FROM Beete WHERE Name = @Name";
+
+                using (SQLiteCommand cmdId = new SQLiteCommand(sqlId, conn))
+                {
+                    cmdId.Parameters.AddWithValue("@Name", beetName);
+                    beetId2 = (int)(long)cmdId.ExecuteScalar();
+                }
+
+                string sqlBelegung = "SELECT * FROM BeetBelegung WHERE BeetId = @BeetId";
+
+                using (SQLiteCommand cmdBelegung = new SQLiteCommand(sqlBelegung, conn))
+                {
+                    cmdBelegung.Parameters.AddWithValue("@BeetId", beetId2);
+                    using (SQLiteDataReader readerBelegung = cmdBelegung.ExecuteReader())
+                    {
+                        while (readerBelegung.Read())
+                        {
+                            int zeile = (int)(long)readerBelegung["Zeile"];
+                            int spalte = (int)(long)readerBelegung["Spalte"];
+                            string pflanzenName = readerBelegung["PflanzenName"].ToString();
+
+                            Pflanze p = pflanzenListe.Find(x => x.Name == pflanzenName);
+                            if (p != null)
+                            {
+                                zellenGrid[zeile, spalte].Background =
+                                    new SolidColorBrush(Color.FromRgb(p.FarbeR, p.FarbeG, p.FarbeB));
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+       
+        
     }
 }
